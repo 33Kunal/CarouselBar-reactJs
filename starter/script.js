8833d147cf6d6b7e8e729aab4b04183b0b8c88e6
Kunal Giradkar <92268598+33Kunal@users.noreply.github.com>
2022-07-19 17:10:00 +0530

    Delete starter directory

--- starter/script.js
+++
@@ -1,9 +0,0 @@
-`strict mode`;
-function kg(ad, kd) {
-    // console.log(`my name is kunal`)
-    const Exs = `kunal has ${ad} year relationship with ad and ${kd} relationship with kd and fucked his teenage life`;
-    return Exs;
-};
-console.log(kg(1, 5));
-console.log(kg(2, 4));
-console.log(kg(3,3));
