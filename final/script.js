3aea32a861e214ea189cb250a330627d081ba99f
Kunal <33kunalg@gmail.com>
2022-12-29 08:47:50 +0530

    updated script

--- final/script.js
+++ final/script.js
@@ -191,10 +191,10 @@ const calcAge = function (birthYeah) {
 }
 const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
